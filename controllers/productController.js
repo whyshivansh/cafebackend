75b6556d9a33cef369bae1ca4865fe1b0322924c
nairx <nairxprv@gmail.com>
2025-07-15 09:14:17 +0530

    updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,4 +11,58 @@ const addProduct = async (req, res) => {
   }
 };
 
-export { addProduct };
+const deleteProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await productModel.findByIdAndDelete(id);
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const result = await productModel.findByIdAndUpdate(id, body);
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+};
+
+const getProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await productModel.findOne({ _id: id });
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+};
+
+const showProducts = async (req, res) => {
+  try {
+    const { page = 1, limit = 3, search = "" } = req.query;
+    const skip = (page - 1) * limit;
+    const count = await productModel.countDocuments({
+      productName: { $regex: search, $options: "i" },
+    });
+    const total = Math.ceil(count / limit);
+    const products = await productModel
+      .find({ productName: { $regex: search, $options: "i" } })
+      .skip(skip)
+      .limit(limit)
+      .sort({ updatedAt: -1 });
+    res.status(200).json({ products, total });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export { addProduct, deleteProduct, updateProduct, getProduct, showProducts };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { addProduct } from "../controllers/productController.js";
+import { addProduct,showProducts,deleteProduct,updateProduct,getProduct } from "../controllers/productController.js";
 
 const Router = express.Router();
 
+
+//admin routes
+Router.get("/", showProducts);
 Router.post("/", addProduct);
+Router.get("/:id", getProduct);
+Router.patch("/:id", updateProduct);
+Router.delete("/:id", deleteProduct);
 
 export default Router;
